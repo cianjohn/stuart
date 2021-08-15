1e4cf7a2577eb8b6b015d5731e4d712e232f0b73
cianjohn <83083026+cianjohn@users.noreply.github.com>
2021-08-15 21:31:37 +0100

    started adding js for dynamic blog posts

---
+++ Blog/js/script.js
@@ -0,0 +1,22 @@
+let card = `<article class="row">
+<div class="col">
+  <div class="card">
+    <h5 class="card-header">Featured</h5>
+    <div class="card-body">
+      <p></p>
+    </div>
+  </div>
+</div>
+</article>`
+
+let posts = {
+    1: {
+        id: 1,
+        title: "hi",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio rerum voluptas saepe quas suscipit perspiciatis magni vel pariatur deleniti inventore, possimus quibusdam accusantium tempora dignissimos aliquam ratione labore ex? Officiis.",
+        date: "15/08/2021"
+    },
+    2:{
+
+    }
+}
